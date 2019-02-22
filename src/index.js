7ba72f6199825c28334c1a6a4e84846cb5b5aed3
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2019-02-22 12:19:42 -0600

    added geolocation API

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+   // call to the geolocation API
+   window.navigator.geolocation.getCurrentPosition(
+      (position ) => console.log(position),
+      (err) => console.log(err)
+   );
    return <div>Hello!</div>
 };
 
