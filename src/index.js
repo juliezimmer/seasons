fec00f686f73dad0413021d18b97d71172a6a884
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2019-02-24 11:16:30 -0600

    added CSS and a loading icon

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
    constructor(props) {
@@ -26,7 +27,7 @@ class App extends React.Component {
       if (!this.state.lat && this.state.errorMessage) {
          return <div>Error: {this.state.errorMessage}</div>;
       } 
-      return <div>Loading!</div>;
+      return <Spinner />;
    }
 }
 
