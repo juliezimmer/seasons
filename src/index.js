b6233bca7a54accb8183a5525aeec62ad1fd72ea
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2019-02-24 09:56:10 -0600

    used semantic UI icons and added text for seasons

--- src/index.js
+++ src/index.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
    constructor(props) {
       super(props);
 
       this.state = {
-         lat: null
+         lat: null,
+         errorMessage: ''
       };
-
-      window.navigator.geolocation.getCurrentPosition(
-         (position ) => {
-            this.setState({ lat: position.coords.latitude});
-         },
-         (err) => console.log(err)
-      );
    }
-        
+
+   componentDidMount() {
+      window.navigator.geolocation.getCurrentPosition(
+         position  => this.setState({ lat: position.coords.latitude}),
+         err => this.setState({ errorMessage: err.message})
+      );
+   } 
+
    render () {
-      return <div>Latitude:  {this.state.lat}</div>;
-   }  
+      if ( this.state.lat && !this.state.errorMessage) {
+         return <SeasonDisplay lat={this.state.lat} />
+      }
+      if (!this.state.lat && this.state.errorMessage) {
+         return <div>Error: {this.state.errorMessage}</div>;
+      } 
+      return <div>Loading!</div>;
+   }
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
